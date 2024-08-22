891fb7d5d5968e45112c9d004c401cd5f2660b12
stormytuna <stormytuna@outlook.com>
2024-08-22 16:50:02 +0100

    Add visuals and sound for immovable elite

--- Content/EliteVariations/ImmovableElite.cs
+++ Content/EliteVariations/ImmovableElite.cs
@@ -1,5 +1,10 @@
 using EliteEnemies.Common;
+using Microsoft.Xna.Framework;
 using Terraria;
+using Terraria.Audio;
+using Terraria.GameContent;
+using Terraria.ID;
+using Terraria.ModLoader;
 
 namespace EliteEnemies.Content.EliteVariations;
 
@@ -15,6 +20,37 @@ public class ImmovableElite : EliteVariation
     {
         if (ApplyEliteVariation) {
             modifiers.DisableKnockback();
+
+            SoundEngine.PlaySound(SoundID.Item37 with { PitchVariance = 0.3f, Pitch = 0.3f }, npc.Center);
+            int numDust = Main.rand.Next(2, 4);
+            for (int i = 0; i < numDust; i ++) {
+                Dust.NewDust(npc.position, npc.width, npc.height, ModContent.DustType<ImmovableEliteDust>());
+            }
         }
+    }
+}
+
+public class ImmovableEliteDust : ModDust
+{
+    public override void OnSpawn(Dust dust)
+    {
+        dust.noGravity = true;
+        dust.frame = new Rectangle(0, Main.rand.Next(3), 16, 16);
+
+        dust.velocity = Main.rand.NextVector2Circular(1f, 1f) * Main.rand.NextFloat(2f, 4f);
+        dust.scale = Main.rand.NextFloat(1.2f, 1.5f);
+    }
+
+    public override bool Update(Dust dust)
+    {
+        dust.position += dust.velocity;
+        dust.velocity *= 0.94f;
+        dust.scale -= 0.015f;
+
+        if (dust.scale < 0.7f) {
+            dust.active = false;
+        }
+
+        return false;
     }
 }
