a69e274023dc639c7591f9127a929ae8a5b3cc7d
stormytuna <stormytuna@outlook.com>
2025-11-21 18:28:13 +0000

    tweak stinky elite to use debuff style damage over time, fix localisation for stinky elite

--- Content/EliteVariations/StinkyElite.cs
+++ Content/EliteVariations/StinkyElite.cs
@@ -29,7 +29,7 @@ public class StinkyElite : EliteVariation
 				_stinkCloudTimer = 0;
 				
 				Vector2 velocity = Main.rand.NextVector2Circular(3f, 3f);
-				Projectile.NewProjectile(npc.GetSource_FromThis(), npc.Center, velocity, ModContent.ProjectileType<StinkyCloud>(), 10, 0f, Main.myPlayer);
+				Projectile.NewProjectile(npc.GetSource_FromThis(), npc.Center, velocity, ModContent.ProjectileType<StinkyCloud>(), 0, 0f, Main.myPlayer);
 			}
 		}
 	}
@@ -37,27 +37,36 @@ public class StinkyElite : EliteVariation
 
 public class StinkyCloud : ModProjectile
 {
-	public override string Texture {
-		get => $"Terraria/Images/Projectile_{ProjectileID.ToxicCloud}";
+	public bool Active => Projectile.Opacity > 0.35f;
+
+	public override void SetStaticDefaults() {
+		Main.projFrames[Type] = 3;
 	}
 
 	public override void SetDefaults() {
 		Projectile.CloneDefaults(ProjectileID.ToxicCloud);
-		
-		Projectile.hostile = true;
+		Projectile.hostile = false;
 		Projectile.friendly = false;
 		
 		AIType = ProjectileID.ToxicCloud;
 	}
 
+	public override void AI() {
+		if (Projectile.frameCounter == 0) {
+			Projectile.frame = Main.rand.Next(Main.projFrames[Type]);
+			Projectile.frameCounter = 1;
+		}
+	}
+
 	public override bool PreDraw(ref Color lightColor) {
 		var texture = TextureAssets.Projectile[Type].Value;
-		
+
+		var sourceRect = texture.Frame(1, Main.projFrames[Type], 0, Projectile.frame);
 		var drawData = new DrawData {
 			texture = texture,
 			position = (Projectile.Center - Main.screenPosition).Floor(),
-			sourceRect = texture.Frame(),
-			origin = texture.Size() / 2f,
+			sourceRect = sourceRect,
+			origin = sourceRect.Size() / 2f,
 			color = Projectile.GetAlpha(lightColor),
 			rotation = Projectile.rotation,
 			scale = new Vector2(Projectile.scale),
@@ -72,5 +81,31 @@ public class StinkyCloud : ModProjectile
 		drawData.Draw(Main.spriteBatch);
 
 		return false;
+	}
+}
+
+public class StinkyCloudPlayer : ModPlayer
+{
+	public override void UpdateBadLifeRegen() {
+		bool nearStinkyCloud = false;
+		
+		foreach (var projectile in Main.ActiveProjectiles) {
+			if (projectile.ModProjectile is StinkyCloud { Active: true } && projectile.Hitbox.Intersects(Player.Hitbox)) {
+				nearStinkyCloud = true;
+				break;
+			}
+		}
+		
+		if (!nearStinkyCloud) {
+			return;
+		}
+
+		if (Player.lifeRegen > 0) {
+			Player.lifeRegen = 0;
+		}
+
+		Player.lifeRegenTime = 0;
+		Player.lifeRegen -= 36;
+		Player.AddBuff(BuffID.Stinky, 30 * 60);
 	}
 }
